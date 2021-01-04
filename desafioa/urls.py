eecf2434fc873d7f1f412fd8d4fcadad04051e2f
Ekain Susperregi <suspekain@gmail.com>
2021-01-04 21:56:44 +0100

    setup bukaera

--- desafioa/urls.py
+++ desafioa/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('k/', include('kudeaketa.urls')),
+    path('e/', include('django.contrib.auth.urls')),
 ]
---
+++ orrialdea/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OrrialdeaConfig(AppConfig):
+    name = 'orrialdea'
